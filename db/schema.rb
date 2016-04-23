3265fa29f498ee15d48e21a2deb44baa17a4b8e2
Jonathan Girard Viau <girardvjonathan@gmail.com>
2016-04-23 17:02:24 -0400

    Posting comment working

--- db/schema.rb
+++ db/schema.rb
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 20160423194702) do
   create_table "comments", force: :cascade do |t|
     t.text     "content"
     t.integer  "user_id"
-    t.integer  "posts_id"
+    t.integer  "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
